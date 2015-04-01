5bb0c64748526505b2d3e3b32c64ff48c4383f3c
Elodie <>
2015-04-01 08:36:58 +0200

    Init by elo

---
+++ src/smtpclient/SMTPClient.java
@@ -0,0 +1,20 @@
+ package smtpclient;
+
+public class SMTPClient {
+    private final int mPortServ;
+    private final String mAdrServ;
+    
+    public SMTPClient(String adrServ, int portServ) {
+        mPortServ = portServ;
+        mAdrServ = adrServ;
+    }
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Elodie Elodie Elodie Mourier");
+    }
+}
---
+++ src/smtpclient/responses/Response.java
@@ -0,0 +1,10 @@
+package smtpclient.responses;
+
+public class Response {
+    private int code;
+    private String mess;
+
+    public Response() {
+    
+    }
+}
